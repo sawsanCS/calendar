0626cb89f82eddf0bc79b9caa2ee017c70830f6c
Saoussen Slii <38138018+sawsanCS@users.noreply.github.com>
2021-09-27 22:54:46 -0400

    created a simple calendar in a react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+const Calendar = require("tui-calendar"); /* CommonJS */
+require("tui-calendar/dist/tui-calendar.css");
 
+// If you use the default popups, use this.
+require("tui-date-picker/dist/tui-date-picker.css");
+require("tui-time-picker/dist/tui-time-picker.css");
 function App() {
+  const calendar = new Calendar("#calendar", {
+    height: "500px",
+
+    defaultView: "month",
+
+    template: {
+      milestone: function (model) {
+        return (
+          '<span class="calendar-font-icon ic-milestone-b"></span> <span style="background-color: ' +
+          model.bgColor +
+          '">' +
+          model.title +
+          "</span>"
+        );
+      },
+    },
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Trying my first calendar </header>
     </div>
   );
 }
